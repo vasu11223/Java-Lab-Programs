88e80c00b960cb25f72648d7addd0e6320a05adc
VASU <118884063+vasu11223@users.noreply.github.com>
2024-02-21 09:00:49 +0530

    Update Fibonacci.java

--- Java Lab prgms/Fibonacci.java
+++ Java Lab prgms/Fibonacci.java
@@ -1,26 +1,45 @@
-class fibonacci {
-    public static void main(String[] args)
- {
-        int n = 10;
-
-        System.out.println("Fibonacci series up to " + n + " terms:");
-        for (int i = 0; i < n; i++) 
-  {
-            System.out.print(fibonacci(i) + " ");
-        }
-    }
-
-    public static int fibonacci(int n) 
+import java.util.Scanner; class Prog1
 {
-        if (n == 0) {
-            return 0; 
-        }
-          else if (n == 1) 
-  {
-            return 1; 
-        } else
- {
-            return fibonacci(n - 1) + fibonacci(n - 2);
- }
+	public static void main(String args[ ])
+	{
+		Scanner sc=new Scanner(System.in); Fibonacci obj=new Fibonacci ();
+		int a,b,c;
+		System.out.println("Which term you want to print "); 
+		a=sc.nextInt();
+		b=obj.fibo_recursion(a);
+		System.out.println("The "+a+"th Fibonacci term using recursion is: "+b); 
+c=obj.fibo_non_recursion(a);
+		System.out.println("The "+a+"th Fibonacci term using non recursion is: "+c);
+	}
 }
+
+class Fibonacci
+{
+	int a=1,b=1,c;
+	public int fibo_recursion (int n)
+	{
+		if(n == 1)
+			return a; 
+		else if(n == 2)
+			return b; 
+		else
+			return fibo_recursion(n-1) + fibo_recursion(n-2);
+	}
+	public int fibo_non_recursion (int n)
+	{
+		if(n==1) 
+			return a;
+		else if(n==2) 
+			return b;
+		else
+		{
+			for(int i=3 ; i<=n ; i++)
+			{
+				c=a+b; 
+				a=b; 
+				b=c;
+			}
+			return c;
+		}
+	}
 }
