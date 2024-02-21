ea433206d43b02f53ef3e3a4b30fae2e0b071163
VASU <118884063+vasu11223@users.noreply.github.com>
2024-02-21 09:03:37 +0530

    Update FileLab.java

--- Java Lab prgms/FileLab.java
+++ Java Lab prgms/FileLab.java
@@ -1,49 +1,42 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*; 
+import java.util.Scanner; 
+class Experiment8
+{
+	public static void main(String args[])throws Exception
+	{
+		String fn;
+		Scanner sc = new Scanner(System.in); 
+		System.out.println("Enter a file name "); 
+		fn=sc.next();
+		File f1 = new File(fn);
+		System.out.println("File Name: " + f1.getName()); 
+		System.out.println("Absolute Path: " + f1.getAbsolutePath()); 
+		System.out.println(f1.exists() ? "The file exists" : "The file does not exist"); 
+		System.out.println(f1.canWrite() ? "It is writeable" : "It is not writeable"); 
+		System.out.println(f1.canRead() ? "It is readable" : "It is not readable"); 
+		System.out.println(f1.isDirectory() ? "It is a directory " : "It is not a directory"); 
+		System.out.println(f1.isFile() ? "It is normal file" : "It might be a named pipe");
+		System.out.println(f1.isAbsolute() ? "It is an absolute file" : "It is not absolute file"); 
+		System.out.println("File size: " + f1.length() + " Bytes");
+	
+		FileInputStream f = new FileInputStream(f1); 
+		int n=f.available(); 
+		int nl=0,nw=0;
+		char ch;
+		System.out.println("Contents of the file: -\n"); 
+		
 
- class FileInfo {
-    public static void main(String[] args) {
-        java.util.Scanner scanner = new java.util.Scanner(System.in);
-        System.out.print("Enter the file name: ");
-        String fileName = scanner.nextLine();
-        File file = new File(fileName);
-
-        if (file.exists()) {
-            System.out.println("File exists.");
-
-            if (file.canRead()) {
-                System.out.println("File is readable.");
-            } else {
-                System.out.println("File is not readable.");
-            }
-
-            if (file.canWrite()) {
-                System.out.println("File is writable.");
-            } else {
-                System.out.println("File is not writable.");
-            }
-
-            if (file.isDirectory()) {
-                System.out.println("File is a directory.");
-            } else if (file.isFile()) {
-                System.out.println("File is a regular file.");
-            }
-
-            System.out.println("File length: " + file.length() + " bytes");
-
-            try (FileInputStream fis = new FileInputStream(file)) {
-                System.out.println("File content:");
-                int data;
-                while ((data = fis.read()) != -1) {
-                    System.out.print((char) data);
-                }
-            } catch (IOException e) {
-                System.out.println("Error reading file: " + e.getMessage());
-            }
-        } else {
-            System.out.println("File does not exist.");
-        }
-        scanner.close();
-    }
+for(int i=0;i<n;i++)
+		{
+			ch=(char)f.read(); 
+			System.out.print(ch); 
+			if(ch=='\n')
+				nl++;
+			else if(ch==' ') 
+				nw++;
+		}
+		System.out.println("\nNumber of lines : "+nl); 
+		System.out.println("\nNumber of words : "+(nl+nw)); 
+		System.out.println("\nNumber of characters : "+n);
+	}
 }
